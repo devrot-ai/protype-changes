48f5cd9953c245f53d145632fe7ee5c99b7ee8ed
kanishk chhoker <kanishkthakur12006@gmail.com>
2026-08-17 13:56:34 +0530

    Add: find-in-mountain-array - submission-0

---
+++ Data Structures & Algorithms/find-in-mountain-array/submission-0.py
@@ -0,0 +1,17 @@
+# """
+# This is MountainArray's API interface.
+# You should not implement it, or speculate about its implementation
+# """
+#class MountainArray:
+#    def get(self, index: int) -> int:
+#    def length(self) -> int:
+
+class Solution:
+    def findInMountainArray(self, target: int, mountainArr: 'MountainArray') -> int:
+        n = mountainArr.length()
+
+        for i in range(n):
+            if mountainArr.get(i) == target:
+                return i
+
+        return -1

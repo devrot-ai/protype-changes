a2983eedf4d4e3ba3531e6d909c5c2b7b1ef51fd
kanishk chhoker <kanishkthakur12006@gmail.com>
2026-09-17 16:46:20 +0530

    Add: triangle - submission-0

---
+++ Data Structures & Algorithms/triangle/submission-0.py
@@ -0,0 +1,8 @@
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        def dfs(row, col):
+            if row >= len(triangle):
+                return 0
+            return triangle[row][col] + min(dfs(row + 1, col), dfs(row + 1, col + 1))
+
+        return dfs(0, 0)
